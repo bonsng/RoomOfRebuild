3a606f57f90ca65a73fdcade8874b96440ea6cfa
bonsng <john.k7795@gmail.com>
2025-01-01 19:10:07 +0900

    main page carousel datas

---
+++ ui/MainPage/carouselItem.ts
@@ -0,0 +1,23 @@
+export type CarouselItem = {
+  id?: number;
+  text: string;
+  img: string;
+};
+
+export const carouselItems: CarouselItem[] = [
+  {
+    id: 0,
+    text: "room of main page carousel item 1",
+    img: "imgurl",
+  },
+  {
+    id: 1,
+    text: "room of main page carousel item 2",
+    img: "imgurl",
+  },
+  {
+    id: 2,
+    text: "room of main page carousel item 3",
+    img: "imgurl",
+  },
+];
