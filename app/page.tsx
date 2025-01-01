2c525d831c42c2365bcc1033753ec6599bf50ad0
bonsng <john.k7795@gmail.com>
2025-01-01 19:11:06 +0900

    add carousel part

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
+import Carousel from "@/ui/MainPage/Carousel";
+
 export default function Home() {
   return (
-    <main className="pt-[1.5rem] lg:pt-0 bg-rice-cake h-[1000px]">
-      <div className="lg:flex lg:flex-row-reverse h-[550px]">
-        <div className="lg:w-1/2 bg-slate-600 ">show room</div>
-        <div className="lg:w-1/2 py-20 px-10">
-          <h1 className="text-4xl">RoomOf</h1>
-          <p>aslkdjf</p>
-        </div>
-      </div>
+    <main className="relative pt-[1.5rem] lg:pt-0 bg-rice-cake h-[1000px] w-full overflow-x-hidden">
+      {/* Carousel Part */}
+      <Carousel />
     </main>
   );
 }
