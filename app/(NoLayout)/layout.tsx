11ef01fb4675bee8bfe968ad81e3324b43764e21
bonsng <john.k7795@gmail.com>
2025-01-04 10:40:54 +0900

    layout change (01.04)

--- app/(NoLayout)/layout.tsx
+++ app/(NoLayout)/layout.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "RoomOf ExampleRoom",
+  description: "Example room of RoomOf service.",
+};
 
 export default function ExampleRoomLayout({
   children,
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,20 @@
 import type { Metadata } from "next";
-import GlobalNav from "./global-nav";
-import Footer from "./footer";
+import "./globals.css";
+import "./globalicon.css";
 
 export const metadata: Metadata = {
   title: "RoomOf",
   description: "Room of whom you miss.",
 };
 
-export default function MainLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <GlobalNav />
-      <main className="pt-[2.5rem] z-0">{children}</main>
-      <Footer />
-    </>
+    <html lang="en">
+      <body className="overflow-y-auto ">{children}</body>
+    </html>
   );
 }
