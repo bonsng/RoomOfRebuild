11a8cf9d3fdef19d645bd1a26a8cd0cc031325a9
bonsng <john.k7795@gmail.com>
2025-02-08 12:08:51 +0900

    layout fix (02.08)

---
+++ ui/ExampleRoom/ExampleRoom3D.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience";
+import { useCameraViewState } from "../CameraView/cameraView.provider";
+import { angles } from "@/util/data/angles";
+import { useEffect } from "react";
+import CameraControls from "./Camera";
+
+export default function ExampleRoom3D() {
+  const { state, dispatch } = useCameraViewState();
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      dispatch({ type: "SET_VIEW", payload: 1 });
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <Canvas
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 2000,
+        position: [0, 11, 150],
+      }}
+    >
+      <Experience />
+      <CameraControls
+        position={angles[state.view].position}
+        target={angles[state.view].target}
+      />
+    </Canvas>
+  );
+}
+
+// [20, 15, 50]
---
+++ ui/ExampleRoom/ExampleRoomHTML.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import { useCameraViewState } from "../CameraView/cameraView.provider";
+import RoomButtons from "./RoomButtons";
+import useMousePosition from "@/util/custom-hook/use-mouse-position";
+
+export default function ExampleRoomHTML() {
+  const { ratio } = useMousePosition();
+  const { state, dispatch } = useCameraViewState();
+
+  const [leftVisible, setLeftVisible] = useState(false);
+  const [rightVisible, setRightVisible] = useState(false);
+
+  const clickLeft = () => {
+    dispatch({
+      type: "SET_VIEW",
+      payload: state.view - 1 == -1 ? 2 : state.view - 1,
+    });
+    console.log(state.view);
+  };
+  const clickRight = () =>
+    dispatch({ type: "SET_VIEW", payload: (state.view + 1) % 3 });
+
+  const clickExit = () => {
+    dispatch({ type: "SET_VIEW", payload: 2 });
+    dispatch({ type: "TOGGLE_FIX" });
+  };
+
+  useEffect(() => {
+    if (ratio) {
+      if (ratio < 0.2) {
+        setLeftVisible(true);
+      } else if (ratio > 0.8) {
+        setRightVisible(true);
+      } else {
+        if (leftVisible || rightVisible) {
+          setLeftVisible(false);
+          setRightVisible(false);
+        }
+      }
+    }
+  }, [ratio]);
+
+  return (
+    <>
+      {state.view < 3 && state.view !== 0 && (
+        <RoomButtons onClick={clickLeft} isLeft={true} visible={leftVisible} />
+      )}
+      {state.view < 3 && state.view !== 2 && (
+        <RoomButtons
+          onClick={clickRight}
+          isLeft={false}
+          visible={rightVisible}
+        />
+      )}
+
+      {state.view === 4 ? (
+        <div className="absolute bottom-10 w-screen flex justify-center bg-transparent">
+          <span
+            onClick={clickExit}
+            className="material-symbols-outlined text-5xl bg-[#5c5c5c3a] rounded-2xl hover:cursor-pointer hover:bg-[rgb(148,148,148)] transition-colors"
+          >
+            close
+          </span>
+        </div>
+      ) : null}
+    </>
+  );
+}
